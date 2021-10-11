7337a5b6c6ebd1f96804e143037c98760601632b
SPS <shyam.sitaula@oit.com.np>
2021-10-11 16:15:50 +0545

    dashboard fe change

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,6 +8,92 @@
 
 @section('content')
   <div class="row">
+
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+
+          <div class="text-secondary">
+            <div class="float-left">
+              SCHOOL
+            </div>
+            <div class="float-right">
+              <i class="fas fa-school fa-2x"></i>
+            </div>
+            <div class="clearfix">
+            </div>
+          </div>
+
+          <div class="h3">
+            SNJES
+          </div>
+
+          <div>
+            <span class="badge badge-primary">School</span>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+
+          <div class="text-secondary">
+            <div class="float-left">
+              SESSION
+            </div>
+            <div class="float-right">
+              <i class="fas fa-calendar fa-2x"></i>
+            </div>
+            <div class="clearfix">
+            </div>
+          </div>
+
+          <div class="h3">
+            2078
+          </div>
+
+          <div>
+            <span class="badge badge-success">Current</span>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="card">
+        <div class="card-body">
+
+          <div class="text-secondary">
+            <div class="float-left">
+              STUDENTS
+            </div>
+            <div class="float-right">
+              <i class="fas fa-user-graduate fa-2x"></i>
+            </div>
+            <div class="clearfix">
+            </div>
+          </div>
+
+          <div class="h3">
+            2213
+          </div>
+
+          <div>
+            <span class="badge badge-danger">Total</span>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+
+  <div class="row">
     <div class="col-md-8">
 
       @if (false)
@@ -20,86 +106,14 @@
         @livewire ('fees-structure-component')
         @livewire ('academic-session-display', ['academicSession' => $academicSession,])
         @livewire ('section-component')
+        @livewire ('sms-credit-component')
       @endif
-
-      @livewire ('sms-credit-component')
-
-
-      @if (false)
-      <div class="card">
-        <div class="card-header border-0">
-          <h2 class="card-title">
-            Session 2078
-          </h2>
-        </div>
-        <div class="card-body">
-
-          <div class="row">
-
-            <div class="col-md-3">
-              <div class="card text-white bg-primary">
-                <div class="card-header">
-                  <h3 class="card-title">
-                    Classes
-                  </h3>
-                </div>
-                <div class="card-body h1">
-                  {{ $academicSession->getTotalClasses() }}
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-3">
-              <div class="card text-white bg-success">
-                <div class="card-header">
-                  <h3 class="card-title">
-                    Sections
-                  </h3>
-                </div>
-                <div class="card-body h1">
-                  {{ $academicSession->getTotalSections() }}
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-3">
-              <div class="card text-white bg-info">
-                <div class="card-header">
-                  <h3 class="card-title">
-                    Student
-                  </h3>
-                </div>
-                <div class="card-body h1">
-                  {{ $academicSession->getTotalStudents() }}
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-3">
-              <div class="card text-white bg-warning">
-                <div class="card-header">
-                  <h3 class="card-title">
-                    Teachers
-                  </h3>
-                </div>
-                <div class="card-body h1">
-                  {{ count($teachers) }}
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-        </div>
-      </div>
-      @endif
-
-
-
     </div>
 
     <div class="col-md-4">
+        @if (false)
         @livewire ('school-component')
+        @endif
     </div>
   </div>
 @stop
